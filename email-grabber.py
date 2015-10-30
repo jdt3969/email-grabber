9050cf6b43e11b3d0a73e60d5f5b6f75fb09985b
jdt3969 <jdt3969@umd.edu>
2015-10-30 12:29:28 -0400

    Allow multiple email checks

--- email-grabber.py
+++ email-grabber.py
@@ -60,47 +60,52 @@ def main():
 		return
 
 
-	inputs = raw_input().lower().split()
+	while True:
+		
+		inputs = raw_input().lower().split()
 
-	# The Eric Tyler Exception
-	if inputs[0] == "eric" and inputs[1] == "tyler" and inputs[2] == "gmail.com":
-		print "Found!\t" + inputs[0] + " " + inputs[1] + "\t" + "bigassmuffin@gmail.com"
-		return
-
-	perms = email_permutator(inputs[0], inputs[1])
-
-	found = False
-
-	for local_part in perms:
-		curl_call = get_curl_call(oauth_token, local_part, inputs[2])
-		buffer = StringIO()
-		c = pycurl.Curl()
-		c.setopt(c.URL, curl_call[0])
-		c.setopt(c.WRITEDATA, buffer)
-		c.setopt(c.HTTPHEADER, curl_call[1:])
-		c.perform()
-		c.close()
-
-		info = buffer.getvalue()
-		parsed_info = json.loads(info)
-
-		if 'firstName' in parsed_info:
-			found = True
-			print "Found!\t" + inputs[0] + " " + inputs[1] + "\t" + local_part + "@" + inputs[2]
-			break
-		elif parsed_info['message'][:20] == "Couldn't find member":
-			print "X " + local_part + "@" + inputs[2]
-		elif parsed_info['message'][:29] == "[unauthorized]. token expired":
-			print "Token Expired -- Exiting"
-			return
-		else:
-			print "Unknown Error -- Exiting"
+		if inputs == ["quit"]:
 			return
 
-		time.sleep(1)
+		# The Eric Tyler Exception
+		if inputs[0] == "eric" and inputs[1] == "tyler" and inputs[2] == "gmail.com":
+			print "Found!\t" + inputs[0] + " " + inputs[1] + "\t" + "bigassmuffin@gmail.com"
+			return
 
-	if not found:
-		print "Not Found"
+		perms = email_permutator(inputs[0], inputs[1])
+
+		found = False
+
+		for local_part in perms:
+			curl_call = get_curl_call(oauth_token, local_part, inputs[2])
+			buffer = StringIO()
+			c = pycurl.Curl()
+			c.setopt(c.URL, curl_call[0])
+			c.setopt(c.WRITEDATA, buffer)
+			c.setopt(c.HTTPHEADER, curl_call[1:])
+			c.perform()
+			c.close()
+
+			info = buffer.getvalue()
+			parsed_info = json.loads(info)
+
+			if 'firstName' in parsed_info:
+				found = True
+				print "Found!\t" + inputs[0] + " " + inputs[1] + "\t" + local_part + "@" + inputs[2]
+				break
+			elif parsed_info['message'][:20] == "Couldn't find member":
+				print "X " + local_part + "@" + inputs[2]
+			elif parsed_info['message'][:29] == "[unauthorized]. token expired":
+				print "Token Expired -- Exiting"
+				return
+			else:
+				print "Unknown Error -- Exiting"
+				return
+
+			time.sleep(1)
+
+		if not found:
+			print "Not Found\t" + inputs[0] + " " + inputs[1]
 
 if __name__ == "__main__":
 	main()
